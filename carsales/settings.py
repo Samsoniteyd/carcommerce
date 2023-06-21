d2b73e465dd83cb96ba2de61fcf3a7e6626c919f
AMOO <amoosamson@gmail.com>
2023-06-21 03:35:22 +0100

    update

--- carsales/settings.py
+++ carsales/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = str(os.getenv('DUMMY_KEY'))
 # DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.206.249.180', 'localhost', '*', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['', 'localhost', '*', '127.0.0.1:8000']
 
 
 # Application definition
