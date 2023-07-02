459f3305d25887f534e693965ec312c0a49377dc
AMOO <amoosamson@gmail.com>
2023-07-02 20:47:35 +0100

    update

--- carsales/settings.py
+++ carsales/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('DUMMY_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
-# DEBUG = True
+# DEBUG = True  
 
 ALLOWED_HOSTS = ['13.211.175.148', 'localhost', '*', '127.0.0.1:8000']
 
