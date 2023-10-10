5bb1784b9469b894175a3e603eb90ae597f690e1
AMOO <amoosamson@gmail.com>
2023-10-10 01:01:29 +0100

    ip added

--- carsales/settings.py
+++ carsales/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = str(os.getenv('DUMMY_KEY'))
 DEBUG = False 
 # DEBUG = True  
 
-ALLOWED_HOSTS = ['3.27.111.97', 'localhost', '*', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['54.252.235.248', 'localhost', '*', '127.0.0.1:8000']
 
 
 # Application definition
